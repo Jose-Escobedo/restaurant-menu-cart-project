d02766c041a4f4a09d3704f742cc6a3c127194ef
+ <escobedo.jose48@outlook.com>
2022-06-30 17:39:49 -0700

    author change

--- src/components/About.js
+++ src/components/About.js
@@ -7,18 +7,11 @@ const About = () => {
       <div className="img-about">
         <img id="spaghetti" src="./images/spaghetti.jpeg"></img>
         <p>
-          Enim neque volutpat ac tincidunt vitae semper quis lectus. Eu non diam
-          phasellus vestibulum lorem sed risus ultricies. Dui nunc mattis enim
-          ut tellus elementum sagittis vitae et. Ullamcorper a lacus vestibulum
-          sed arcu non odio euismod. Pellentesque adipiscing commodo elit at
-          imperdiet dui accumsan sit. Tincidunt vitae semper quis lectus.
-          Phasellus faucibus scelerisque eleifend donec pretium. Proin sed
-          libero enim sed faucibus turpis in eu. Nisl suscipit adipiscing
-          bibendum est ultricies integer. Sollicitudin tempor id eu nisl nunc mi
-          ipsum. In ornare quam viverra orci sagittis eu volutpat odio
-          facilisis. Ut tristique et egestas quis. Hendrerit gravida rutrum
-          quisque non. Scelerisque in dictum non consectetur a erat nam at.
-          Metus dictum at tempor commodo ullamcorper.
+          Welcome to Frank and Jose’s Italian Kitchen! Our restaurant has been a
+          family operated restaurant since 2022. We offer traditional Italian
+          pasta dishes along with our famous garlic bread and thin crust pizza.
+          Our friendly staff and warm atmosphere makes Frank and Jose’s the
+          perfect place for your friends and family.
         </p>
       </div>
     </div>
